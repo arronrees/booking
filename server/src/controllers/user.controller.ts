d81a1c8298765beb6abf78d70ce13c85265a73c7
arronrees <64908445+arronrees@users.noreply.github.com>
2022-12-13 18:25:06 +0000

    Added update user address handler

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import { prismaDB } from '..';
 import checkValidUuid from '../utils/checkValidUuid';
 
-export async function getSingleUser(req: Request, res: Response) {
+export async function getSingleUserController(req: Request, res: Response) {
   try {
     const { userId } = req.params;
 
@@ -17,6 +17,35 @@ export async function getSingleUser(req: Request, res: Response) {
     }
 
     return res.status(200).json({ success: true, data: user });
+  } catch (err) {
+    console.error(err);
+
+    return res.status(500).json({
+      success: false,
+      error: err,
+    });
+  }
+}
+
+export async function updateUserAddressController(req: Request, res: Response) {
+  try {
+    const { addressId } = req.params;
+    const { address } = req.body;
+
+    if (!checkValidUuid(addressId)) {
+      return res
+        .status(404)
+        .json({ success: false, error: 'Address not found' });
+    }
+
+    const updatedAddress = await prismaDB.address.update({
+      where: { id: addressId },
+      data: {
+        ...address,
+      },
+    });
+
+    res.status(200).json({ success: true, data: updatedAddress });
   } catch (err) {
     console.error(err);
 
--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -1,7 +1,18 @@
 import { Router } from 'express';
-import { getSingleUser } from '../controllers/user.controller';
+import {
+  getSingleUserController,
+  updateUserAddressController,
+} from '../controllers/user.controller';
+import { checkAddressObjectValid } from '../middleware/address.middleware';
 
 export const userRouter = Router();
 
 // get user information
-userRouter.get('/:userId', getSingleUser);
+userRouter.get('/:userId', getSingleUserController);
+
+// update user address
+userRouter.put(
+  '/update-address/:addressId',
+  checkAddressObjectValid,
+  updateUserAddressController
+);
