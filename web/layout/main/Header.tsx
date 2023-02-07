8ae9309c99ec3771b537a841a5ce82a8837a7142
arronrees <64908445+arronrees@users.noreply.github.com>
2023-02-07 19:50:11 +0000

    Updated Header

--- web/layout/main/Header.tsx
+++ web/layout/main/Header.tsx
@@ -1,10 +1,14 @@
 import { Bars3Icon } from '@heroicons/react/24/solid';
 import Link from 'next/link';
 import { useState } from 'react';
+import { UserInterface } from '../../constant-types';
+import useUser from '../../utils/iron/useUser';
 import DividerLine from './DividerLine';
 
 export default function Header() {
   const [displayNav, setDisplayNav] = useState(false);
+
+  const { user }: { user: UserInterface } = useUser();
 
   return (
     <div className='p-6'>
@@ -15,9 +19,22 @@ export default function Header() {
         >
           YourTicket
         </Link>
+        <div className='ml-auto'>
+          {user ? (
+            <Link href='/user/profile'>
+              <div className='flex items-center justify-center w-10 h-10 rounded-full border-2 border-white bg-gray-400 text-dark-blue font-bold'>
+                <span>{user.name[0]}</span>
+              </div>
+            </Link>
+          ) : (
+            <Link href='/auth/signin' className='btn btn--blue'>
+              Sign In
+            </Link>
+          )}
+        </div>
         <button
           type='button'
-          className='cursor-pointer relative z-50 lg:hidden ml-auto xs:ml-0'
+          className='cursor-pointer relative z-50 lg:hidden xs:ml-0'
           onClick={() => setDisplayNav(!displayNav)}
         >
           <Bars3Icon className='w-8 h-8' />
